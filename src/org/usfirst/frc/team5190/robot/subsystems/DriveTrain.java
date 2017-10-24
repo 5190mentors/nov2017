555067daa04d2e2f8f3fb19b67747e98c42aa6e3
5190mentors <mat.esc@outlook.com>
2017-10-24 18:24:03 -0400

    Initial version of balancing code

--- src/org/usfirst/frc/team5190/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team5190/robot/subsystems/DriveTrain.java
@@ -1,11 +1,14 @@
 package org.usfirst.frc.team5190.robot.subsystems;
 
+import org.usfirst.frc.team5190.robot.Robot;
 import org.usfirst.frc.team5190.robot.commands.ArcadeDriveWithJoystick;
 
+import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -22,6 +25,12 @@ public class DriveTrain extends Subsystem {
 	private static final int FRONT_RIGHT = 1;
 	private static final int REAR_RIGHT = 3;
 	
+	private static final double GYRO_CORRECTION_CONSTANT = 1;
+	private static final double GYRO_RESPONSE_DELAY = 0.004;
+	private static final double BALANCING_MIN_TIME_CHECK = 1;
+	private static final double BALANCING_MAX_TIME_CHECK = 20;
+	private static final double BALANCING_TOLERANCE_LIMIT = 5;
+	
 	private SpeedController frontLeftMotor = new Jaguar(FRONT_LEFT);
 	private SpeedController rearLeftMotor = new Jaguar(REAR_LEFT);
 	private SpeedController frontRightMotor = new Jaguar(FRONT_RIGHT);
@@ -33,6 +42,7 @@ public class DriveTrain extends Subsystem {
 //	private Encoder rightEncoder = new Encoder(3, 4);
 //	private AnalogInput rangefinder = new AnalogInput(6);
 //	private AnalogGyro gyro = new AnalogGyro(1);
+	private Timer balancingTimer = new Timer();
 
 	public DriveTrain() {
 		super();
@@ -50,7 +60,7 @@ public class DriveTrain extends Subsystem {
 //			leftEncoder.setDistancePerPulse((4.0 / 12.0 * Math.PI) / 360.0);
 //			rightEncoder.setDistancePerPulse((4.0 / 12.0 * Math.PI) / 360.0);
 //		}
-
+		
 		// Let's show everything on the LiveWindow
 		LiveWindow.addActuator("Drive Train", "Front_Left Motor", (Jaguar) frontLeftMotor);
 		LiveWindow.addActuator("Drive Train", "Back Left Motor", (Jaguar) rearLeftMotor);
@@ -95,6 +105,7 @@ public class DriveTrain extends Subsystem {
 
 	public void reset() {
 //		gyro.reset();
+		balancingTimer.reset();
 //		leftEncoder.reset();
 //		rightEncoder.reset();
 	}
@@ -108,5 +119,19 @@ public class DriveTrain extends Subsystem {
 //		// Really meters in simulation since it's a rangefinder...
 //		return rangefinder.getAverageVoltage();
 		return 0;
+	}
+	
+	public void autoBalanceOnTeeterTotter() {
+//		double pitchAngleDegrees = getHeading();
+//        double pitchAngleRadians = pitchAngleDegrees * (Math.PI / 180.0);
+//        double yAxisRate = Math.sin(pitchAngleRadians) * GYRO_CORRECTION_CONSTANT;
+//        drive(yAxisRate, 0);
+//        Timer.delay(GYRO_RESPONSE_DELAY);		// wait for a motor update time    
+    }
+	
+	public boolean isBalanced() {
+		double elapsed = balancingTimer.get();
+		return elapsed > BALANCING_MAX_TIME_CHECK || 
+				(elapsed > BALANCING_MIN_TIME_CHECK && getHeading() < BALANCING_TOLERANCE_LIMIT);
 	}
 }
